b9fdac12c1efecec1a08467231a24bb4304836ec
Rasha-Gamal <44783930+Rasha-Gamal@users.noreply.github.com>
2018-11-16 15:19:56 +0200

    intial

---
+++ task1.1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using task1._1.Models;
+
+namespace task1._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index()
+        {
+            return "welcome to home page";
+        }
+        public String Welcome()
+        {
+            return "this is the welcome page ";
+        }
